5d0d719506e79b642757ae87ba1337b9fb6b742f
AJ Thompson <thoantj@dunwoody.edu>
2018-05-02 09:01:41 -0500

    first commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,126 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
+
+    class Darkest_project {
+
+    @FXML
+    private VBox window;
+
+    @FXML
+    private AnchorPane Anchorpane;
+
+    @FXML
+    private Rectangle background;
+
+    @FXML
+    private ImageView Logo;
+
+    @FXML
+    private ImageView imageBountyHunter;
+
+    @FXML
+    private ImageView imageAbomination;
+
+    @FXML
+    private ImageView imagePlagueDoctor;
+
+    @FXML
+    private ImageView imageGraveRobber;
+
+    @FXML
+    private ImageView imageLeper;
+
+    @FXML
+    private ImageView imageOccultist;
+
+    @FXML
+    private ImageView imageShieldbreaker;
+
+    @FXML
+    private ImageView imageAntiquarian;
+
+    @FXML
+    private ImageView imageHoundmaster;
+
+    @FXML
+    private ImageView imageArbalest;
+
+    @FXML
+    private ImageView imageCrusader;
+
+    @FXML
+    private ImageView imageManAtArms;
+
+    @FXML
+    private ImageView imageJester;
+
+    @FXML
+    private ImageView imageHellion;
+
+    @FXML
+    private ImageView imageHighwayman;
+
+    @FXML
+    private ImageView imageVestal;
+
+    @FXML
+    private ImageView imageFlagellant;
+
+    @FXML
+    private Label labelAbomination;
+
+    @FXML
+    private Label labelAntiquarian;
+
+    @FXML
+    private Label labelArbalest;
+
+    @FXML
+    private Label labelBountyHunter;
+
+    @FXML
+    private Label labelCrusader;
+
+    @FXML
+    private Label labelFlagellant;
+
+    @FXML
+    private Label labelGraveRobber;
+
+    @FXML
+    private Label labelHellion;
+
+    @FXML
+    private Label labelHighwayman;
+
+    @FXML
+    private Label labelHoundmaster;
+
+    @FXML
+    private Label labelJester;
+
+    @FXML
+    private Label labelLeper;
+
+    @FXML
+    private Label labelManAtArms;
+
+    @FXML
+    private Label labelOccultist;
+
+    @FXML
+    private Label labelPlagueDoctor;
+
+    @FXML
+    private Label labelShieldbreaker;
+
+    @FXML
+    private Label labelVestal;
+
+}
